5c2b841e4b16e316923f1fd94d7b00c5d284d11b
Yancy <softwarexyy@126.com>
2017-03-09 16:12:46 +0800

    create test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年03月09日 星期四 16时08分14秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This is used for learning Git." << endl;
+}
