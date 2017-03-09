2f5653608179d9ff7e44b1a6be06b984300fdf51
Yancy <softwarexyy@126.com>
2017-03-09 17:23:00 +0800

    yancy modify

--- test.cpp
+++ test.cpp
@@ -14,5 +14,6 @@ int main()
     cout << "I am learning git." << endl;
     cout << "I am trying to connect to github." << endl;
     cout << "hello world" << endl;
+    cout << "learn git" << endl;
     return 0;
 }
